222b5d9671f2589a3618a56dedf141f11894e16c
Bruno Alves <bruno-alves2010@live.com>
2022-11-07 21:59:45 -0300

    feat: add tests to ensure basic button behaviors

--- libs/button/src/lib/button.spec.tsx
+++ libs/button/src/lib/button.spec.tsx
@@ -1,10 +1,33 @@
-import { render } from '@testing-library/react';
+import { ThemeProvider } from '@emotion/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 
-import Button from './button';
+import { Button, ButtonProps } from './button';
 
-describe('Button', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<Button />);
-    expect(baseElement).toBeTruthy();
+const renderButton = ({ onClick = jest.fn(), children }: Partial<ButtonProps>): void => {
+  render(
+    <ThemeProvider theme={() => ({ colors: { ...jest.fn(), hover: jest.fn() } })}>
+      <Button variant='destructive' onClick={onClick}>
+        {children}
+      </Button>
+    </ThemeProvider>
+  );
+};
+
+describe('<Button />', () => {
+  test('Should emit a click event when the button receives a click', () => {
+    const onClickSpy = jest.fn();
+
+    renderButton({ onClick: onClickSpy });
+
+    const button = screen.getByRole('button');
+    fireEvent.click(button);
+
+    expect(onClickSpy).toHaveBeenCalled();
+  });
+
+  test('Should render children content', () => {
+    renderButton({ children: 'Im a children content' });
+
+    expect(screen.queryByText('Im a children content')).not.toBeNull();
   });
 });
