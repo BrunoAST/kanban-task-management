9b01a35ae961b701d84aa25168949eabbc5cca98
Bruno Alves <bruno-alves2010@live.com>
2022-11-07 21:56:56 -0300

    docs: add story for button component

--- apps/components-docs/src/stories/button.stories.tsx
+++ apps/components-docs/src/stories/button.stories.tsx
@@ -1,15 +1,54 @@
 import { Button } from '@kanban-task-management/button';
+import { BaseThemeProvider } from '@kanban-task-management/theme';
 
-import { ComponentMeta } from '@storybook/react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
 
 export default {
-  title: 'Button',
+  title: 'Components/Button',
+  parameters: {
+    layout: 'centered',
+  },
+  argTypes: {
+    onClick: {
+      description: 'Button click event',
+      table: {
+        category: 'Events'
+      }
+    },
+    variant: {
+      control: { type: 'select' },
+      description: 'Changes the color of the button and its size',
+      table: {
+        category: 'Property'
+      }
+    },
+    children: {
+      control: { type: 'text' },
+      description: 'Renders a child content',
+      table: {
+        category: 'Property'
+      }
+    },
+  },
+  args: {
+    variant: 'primaryLg',
+    children: 'Button',
+  },
   component: Button
 } as ComponentMeta<typeof Button>;
 
-const Template = ({ title }) => <Button title={title} />;
+const Template: ComponentStory<typeof Button> = ({ variant, children }) => {
+  return (
+    <BaseThemeProvider>
+      <Button
+        onClick={() => console.log('onClick trigged')}
+        variant={variant}
+      >
+        {children}
+      </Button>
+    </BaseThemeProvider>
+  );
+};
 
 export const Primary = Template.bind({});
-Primary.args = {
-  title: 'Ola',
-};
+Primary.args = {};
